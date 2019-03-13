a99b653b6ff19321c85335d09f764843f15020db
munair <munair@gmail.com>
2019-03-14 05:27:09 +0900

    more testing...

--- safebid-sandbox-endpoint/index.js
+++ safebid-sandbox-endpoint/index.js
@@ -81,7 +81,7 @@ async function getaccountinformation(){
     let usdaccountinformation = filter(accountinformation, usdcurrencyfilter);
     let usdavailablebalance = usdaccountinformation[0].available;
     console.log('usdavailablebalance: ' + usdavailablebalance);
-    console.log('The safe bid amount is: $' + usdavailablebalance.toFixed(2));
+    console.log('The safe bid amount is: $' + (usdavailablebalance/100).toFixed(2));
     console.log('exiting...');
   } catch (e) {
     console.error('[ ' + Date() + ' ] ', e);

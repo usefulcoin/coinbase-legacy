c67cce71b3df08a25571967bd3ce8585605f2623
munair <munair@gmail.com>
2019-03-15 03:07:13 +0900

    gradual rollback...

--- safebid-sandbox-endpoint/index.js
+++ safebid-sandbox-endpoint/index.js
@@ -167,7 +167,7 @@ async function sendmessage(alert, number) {
   // create promise and SNS service object
   try{
     let publishedtext = await new aws.SNS({apiVersion: '2010-03-31'}).publish(params);
-    let json = await publishedtext.json();
+    let json = await publishedtext;
     console.log(publishedtext);
   } catch (e) {
     console.error('[ ' + Date() + ' ] ', e);

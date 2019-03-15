e95f73e85d636e50b9aaaaa9a112a07861a27a70
munair <munair@gmail.com>
2019-03-16 01:57:48 +0900

    set stop loss higher than ask price using 10x the absolute increment plus the desired return.

--- submit-safe-bid-ask-pair-to-sandbox-endpoint/index.js
+++ submit-safe-bid-ask-pair-to-sandbox-endpoint/index.js
@@ -200,7 +200,7 @@ async function sendmessage(message, phonenumber) {
       // make ask...
       askquantity = bidquantity;
       askprice = Math.round( bidprice * ( 1 + percentreturn ) / quoteincrement ) * quoteincrement;
-      askstop = Math.round( bidprice * ( 1 + 2*percentreturn ) / quoteincrement ) * quoteincrement;
+      askstop = 10 * quoteincrement + Math.round( bidprice * ( 1 + percentreturn ) / quoteincrement ) * quoteincrement;
       stop = 'loss';
       console.log(askprice,askquantity,'buy',true,productid,stop,askstop);
       let postedask = await postorder(askprice,askquantity,'buy',true,productid,stop,askstop);

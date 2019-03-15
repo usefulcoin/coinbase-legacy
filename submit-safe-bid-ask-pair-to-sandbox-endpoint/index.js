8104304c6e796fdfca4139984d398f78bec5af7b
munair <munair@gmail.com>
2019-03-15 23:37:57 +0900

    turns out orders weren't going through anyway and i was solving a pre-existing problem.

--- submit-safe-bid-ask-pair-to-sandbox-endpoint/index.js
+++ submit-safe-bid-ask-pair-to-sandbox-endpoint/index.js
@@ -60,12 +60,62 @@ function filter(array, filters) {
 
 
 
+// make rest api request...
+async function restapirequest(method,requestpath,body){
+ 
+  // create the prehash string by concatenating required parts of request...
+  let timestamp = Date.now() / 1000;
+  let prehash = timestamp + method + requestpath;
+  if ( body !== undefined ) { prehash = prehash + body; }
+  // created the prehash.
+ 
+  // base64 decode the secret...
+  let base64decodedsecret = Buffer(secret, 'base64');
+  // secret decoded.
+ 
+  // create sha256 hmac with the secret...
+  let hmac = crypto.createHmac('sha256',base64decodedsecret);
+  // created sha256 hmac.
+ 
+  // sign the require message with the hmac and base64 encode the result..
+  let signedmessage = hmac.update(prehash).digest('base64');
+  // signed message.
+ 
+  // define coinbase required headers...
+  let headers = {
+    'ACCEPT': 'application/json',
+    'CONTENT-TYPE': 'application/json',
+    'CB-ACCESS-KEY': key,
+    'CB-ACCESS-SIGN': signedmessage,
+    'CB-ACCESS-TIMESTAMP': timestamp,
+    'CB-ACCESS-PASSPHRASE': passphrase,
+  };
+  // defined coinbase required headers. yes... content-type is required.
+  // see https://docs.prime.coinbase.com/#requests for more information.
+
+  // define request options for http request...
+  let requestoptions = { 'method': method, headers };
+  if ( body !== undefined ) { requestoptions['body'] = body; }
+  // defined request options for http request.
+
+  // define url and send request...
+  let url = 'https://api-public.sandbox.prime.coinbase.com' + requestpath;
+  let response = await fetch(url,requestoptions);
+  let json = await response.json();
+  // defined url and sent request.
+
+  return json;
+}
+// made rest api request.
+
+
+
+
 // post a order...
 async function postorder(price,size,side,postonly,productid,stop,stopprice){
 
-  // create the prehash string by concatenating required parts of request...
+  // define parameters...
   let method = 'POST';
-  let timestamp = Date.now() / 1000;
   let requestpath = '/orders';
   let body = JSON.stringify({
       'price': price,
@@ -76,96 +126,13 @@ async function postorder(price,size,side,postonly,productid,stop,stopprice){
       'stop': stop,
       'stop_price': stopprice
   });
-  let prehash = timestamp + method + requestpath + body;
-  // created the prehash.
+  // defined parameters.
 
-  // base64 decode the secret...
-  let base64decodedsecret = Buffer(secret, 'base64');
-  // secret decoded.
+  let order = await restapirequest(method,requestpath,body);
 
-  // create sha256 hmac with the secret.
-  let hmac = crypto.createHmac('sha256',base64decodedsecret);
-  // created sha256 hmac.
-
-  // sign the require message with the hmac and base64 encode the result..
-  let signedmessage = hmac.update(prehash).digest('base64');
-  // signed message.
-
-  // define coinbase required headers...
-  let headers = {
-    'ACCEPT': 'application/json',
-    'CONTENT-TYPE': 'application/json',
-    'CB-ACCESS-KEY': key,
-    'CB-ACCESS-SIGN': signedmessage,
-    'CB-ACCESS-TIMESTAMP': timestamp,
-    'CB-ACCESS-PASSPHRASE': passphrase,
-  };
-  // defined coinbase required headers. yes... content-type is required.
-  
-
-  // define request options for http request...
-  let requestoptions = { 'method': method.toUpperCase(), 'body': body, headers };
-  // defined request options for http request.
-
-  // define url and send request...
-  let url = 'https://api-public.sandbox.prime.coinbase.com' + requestpath;
-  let response = await fetch(url,requestoptions);
-  let json = await response.json();
-  // defined url and sent request.
-
-  return json;
+  return order;
 }
 // posted a order.
-
-
-
-
-// make a generic GET request...
-async function getrequest(endpoint){
-  
-  // create the prehash string by concatenating required parts of request...
-  let method = 'GET';
-  let timestamp = Date.now() / 1000;
-  let requestpath = '/' + endpoint;
-  let prehash = timestamp + method + requestpath;
-  // created the prehash.
-  
-  // base64 decode the secret...
-  let base64decodedsecret = Buffer(secret, 'base64');
-  // secret decoded.
-  
-  // create sha256 hmac with the secret...
-  let hmac = crypto.createHmac('sha256',base64decodedsecret);
-  // created sha256 hmac.
-  
-  // sign the require message with the hmac and base64 encode the result..
-  let signedmessage = hmac.update(prehash).digest('base64');
-  // signed message.
-  
-  // define coinbase required headers...
-  let headers = {
-    'ACCEPT': 'application/json',
-    'CONTENT-TYPE': 'application/json',
-    'CB-ACCESS-KEY': key,
-    'CB-ACCESS-SIGN': signedmessage,
-    'CB-ACCESS-TIMESTAMP': timestamp,
-    'CB-ACCESS-PASSPHRASE': passphrase,
-  };
-  // defined coinbase required headers. yes... content-type is required.
-  
-  // define request options for http request...
-  let requestoptions = { 'method': method.toUpperCase(), headers };
-  // defined request options for http request.
-  
-  // define url and send request...
-  let url = 'https://api-public.sandbox.prime.coinbase.com' + requestpath;
-  let response = await fetch(url,requestoptions);
-  let json = await response.json();
-  // defined url and sent request.
-
-  return json;
-}
-// made GET request.
 
 
 
@@ -198,7 +165,7 @@ async function sendmessage(message, phonenumber) {
 
     // retrieve product information...
     let productidfilter = { id: [productid] };
-    let productinformation = await getrequest('products');
+    let productinformation = await restapirequest('GET','/products');
     let filteredproductinformation = filter(productinformation, productidfilter);
     let baseminimum = filteredproductinformation[0].base_min_size;
     let basemaximum = filteredproductinformation[0].base_max_size;
@@ -208,14 +175,14 @@ async function sendmessage(message, phonenumber) {
 
     // retrieve available balance information...
     let quotecurrencyfilter = { currency: [quotecurrency] };
-    let accountinformation = await getrequest('accounts');
+    let accountinformation = await restapirequest('GET','/accounts');
     let quoteaccountinformation = filter(accountinformation, quotecurrencyfilter);
     let quoteavailablebalance = quoteaccountinformation[0].available;
     let quoteriskableavailable = quoteavailablebalance*riskratio;
     // retrieved account balance information.
 
     // retrieve product ticker information...
-    let producttickerinformation = await getrequest('/products/' + productid + '/ticker');
+    let producttickerinformation = await restapirequest('GET','/products/' + productid + '/ticker');
     let bid = producttickerinformation.bid;
     // retrieved product ticker information.
 

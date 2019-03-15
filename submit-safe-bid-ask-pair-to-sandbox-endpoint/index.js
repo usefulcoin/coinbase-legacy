c729f15572881c9a6aa09ad05596d182ce1e2e95
munair <munair@gmail.com>
2019-03-16 01:54:58 +0900

    set stop loss higher than ask price.

--- submit-safe-bid-ask-pair-to-sandbox-endpoint/index.js
+++ submit-safe-bid-ask-pair-to-sandbox-endpoint/index.js
@@ -199,8 +199,8 @@ async function sendmessage(message, phonenumber) {
 
       // make ask...
       askquantity = bidquantity;
-      askprice = Math.round( bidprice * ( 1 + 2*percentreturn ) / quoteincrement ) * quoteincrement;
-      askstop = Math.round( bidprice * ( 1 + percentreturn ) / quoteincrement ) * quoteincrement;
+      askprice = Math.round( bidprice * ( 1 + percentreturn ) / quoteincrement ) * quoteincrement;
+      askstop = Math.round( bidprice * ( 1 + 2*percentreturn ) / quoteincrement ) * quoteincrement;
       stop = 'loss';
       console.log(askprice,askquantity,'buy',true,productid,stop,askstop);
       let postedask = await postorder(askprice,askquantity,'buy',true,productid,stop,askstop);

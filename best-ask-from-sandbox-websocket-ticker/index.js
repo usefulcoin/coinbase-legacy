73cda495b9ec93ef8e079b75d23f55ef227966e8
munair <munair@gmail.com>
2019-03-16 14:05:59 +0900

    testing a simple script to connect to the BTC-USD ticker websocket on Coinbase Prime.

---
+++ best-ask-from-sandbox-websocket-ticker/index.js
@@ -0,0 +1,57 @@
+/*
+ * index.js
+ *
+ * Copyright (c) 2019 Useful Coin LLC. All Rights Reserved.
+ *
+ * This file is licensed. You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * https://raw.githubusercontent.com/usefulcoin/coinbase/master/LICENSE
+ *
+ * This script is supposed to submit safe bid and ask pairs to the sandbox. Please read the
+ * README.md file for further information.
+ *
+ */
+
+
+
+
+// load modules...
+const websocket = require('ws');
+// loading complete.
+
+
+
+
+// define consts...
+const ws = new websocket('wss://ws-feed-public.sandbox.prime.coinbase.com');
+// defined key static (const) variables.
+
+
+
+
+// create subscription request...
+subscriptionrequest = {
+    "type": "subscribe",
+    "product_ids": ["BTC-USD"],
+    "channels": ["ticker"]
+}
+// created subscription request...
+
+
+
+
+// on open connection and send subscribe request...
+ws.on('open', function open() {
+  ws.send(subscriptionrequest);
+});
+// opened connection and sent subscribe request...
+
+
+
+
+// update the console when the ticker changes...
+ws.on('message', function incoming(data) {
+  console.log(data);
+});
+// updated the console.

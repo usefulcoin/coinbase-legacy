bf96322ee60dd5ccff99ff145d91ed5918302941
munair <munair@gmail.com>
2019-03-17 14:03:28 +0900

    add code to send SNS message via Amazon.

---
+++ sendmessage/index.js
@@ -0,0 +1,62 @@
+/*
+ * index.js
+ *
+ * Copyright (c) 2019 Useful Coin LLC. All Rights Reserved.
+ *
+ * This file is licensed. You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * https://raw.githubusercontent.com/usefulcoin/coinbase/master/LICENSE
+ *
+ * This script is supposed to submit safe bid and ask pairs to the sandbox. Please read the
+ * README.md file for further information.
+ *
+ */
+
+
+
+
+// load modules...
+const aws = require('aws-sdk');
+// loading complete.
+
+
+
+
+// define consts...
+const recipient = '+12062270634';
+// defined key static (const) variables.
+
+
+
+
+// configure aws region (use ap-northeast-1 or us-west-2)...
+aws.config.update({region: 'ap-northeast-1'});
+// configured aws.
+
+
+
+
+// send a message...
+async function sendmessage(message, phonenumber) {
+  // create publish parameters...
+  let parameters = { Message: message, PhoneNumber: phonenumber };
+  // create publish parameters.
+
+  try{
+    // create promise and SNS service object...
+    let publishedtext = await new aws.SNS({apiVersion: '2010-03-31'}).publish(parameters).promise();
+    console.log('sent message with id: ' + publishedtext.MessageId);
+    // created promise and SNS service object
+  } catch (e) {
+    console.error('[ ' + Date() + ' ] ', e);
+  };
+}
+// sent a message.
+
+
+
+
+(async function main() {
+  try { sendmessage('it is bedtime son.', recipient); } catch(e) { console.error(e); }
+}());

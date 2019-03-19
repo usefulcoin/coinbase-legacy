4f78607a2b8caa4e181a34e4c7ad04a3943dd880
munair <munair@gmail.com>
2019-03-19 12:40:17 +0900

    all set for renewed testing...

--- safe-bid-ticker-until-filled-sandbox-endpoint/index.js
+++ safe-bid-ticker-until-filled-sandbox-endpoint/index.js
@@ -17,8 +17,10 @@
 
 
 // load modules...
+const aws = require('aws-sdk');
 const websocket = require('ws');
 const crypto = require('crypto');
+const fetch = require('node-fetch');
 // loading complete.
 
 
@@ -26,7 +28,10 @@ const crypto = require('crypto');
 
 // define consts...
 const channel = 'ticker';
+const riskratio = 0.0001;
+const percentreturn = 0.01;
 const productid = 'BTC-USD';
+const recipient = '+12062270634';
 const ws = new websocket('wss://ws-feed-public.sandbox.prime.coinbase.com');
 // defined key static (const) variables.
 
@@ -38,6 +43,93 @@ const key = process.env.apikey;
 const secret = process.env.apisecret;
 const passphrase = process.env.apipassphrase;
 // importing of sensitive authentication data complete.
+
+
+
+
+// filter an array of objects...
+function filter(array, filters) {
+  let itemstoinclude = Object.keys(filters);
+  return array.filter((item) => itemstoinclude.every((key) => (filters[key].indexOf(item[key]) !== -1)));
+}
+// filtered array.
+
+
+
+
+// make rest api request...
+async function restapirequest(method,requestpath,body){
+ 
+  // create the prehash string by concatenating required parts of request...
+  let timestamp = Date.now() / 1000;
+  let prehash = timestamp + method + requestpath;
+  if ( body !== undefined ) { prehash = prehash + body; }
+  // created the prehash.
+ 
+  // base64 decode the secret...
+  let base64decodedsecret = Buffer(secret, 'base64');
+  // secret decoded.
+ 
+  // create sha256 hmac with the secret...
+  let hmac = crypto.createHmac('sha256',base64decodedsecret);
+  // created sha256 hmac.
+ 
+  // sign the require message with the hmac and base64 encode the result..
+  let signedmessage = hmac.update(prehash).digest('base64');
+  // signed message.
+ 
+  // define coinbase required headers...
+  let headers = {
+    'ACCEPT': 'application/json',
+    'CONTENT-TYPE': 'application/json',
+    'CB-ACCESS-KEY': key,
+    'CB-ACCESS-SIGN': signedmessage,
+    'CB-ACCESS-TIMESTAMP': timestamp,
+    'CB-ACCESS-PASSPHRASE': passphrase,
+  };
+  // defined coinbase required headers. yes... content-type is required.
+  // see https://docs.prime.coinbase.com/#requests for more information.
+
+  // define request options for http request...
+  let requestoptions = { 'method': method, headers };
+  if ( body !== undefined ) { requestoptions['body'] = body; }
+  // defined request options for http request.
+
+  // define url and send request...
+  let url = 'https://api-public.sandbox.prime.coinbase.com' + requestpath;
+  let response = await fetch(url,requestoptions);
+  let json = await response.json();
+  // defined url and sent request.
+
+  return json;
+}
+// made rest api request.
+
+
+
+
+// post a order...
+async function postorder(price,size,side,postonly,productid,stop,stopprice){
+
+  // define parameters...
+  let method = 'POST';
+  let requestpath = '/orders';
+  let body = JSON.stringify({
+      'price': price,
+      'size': size,
+      'side': side,
+      'post_only': postonly,
+      'product_id': productid,
+      'stop': stop,
+      'stop_price': stopprice
+  });
+  // defined parameters.
+
+  let order = await restapirequest(method,requestpath,body);
+
+  return order;
+}
+// posted a order.
 
 
 
@@ -84,6 +176,33 @@ function channelsubscription(type, productid, channel, signature, key, passphras
   return subscriptionrequest;
 }
 // created or discontinued subscription request.
+
+
+
+
+// configure aws region (use ap-northeast-1 or us-west-2)...
+aws.config.update({region: 'ap-northeast-1'});
+// configured aws.
+
+
+
+
+// send a message...
+async function sendmessage(message, phonenumber) {
+  // create publish parameters...
+  let parameters = { Message: message, PhoneNumber: phonenumber };
+  // create publish parameters.
+
+  try{
+    // create promise and SNS service object...
+    let publishedtext = await new aws.SNS({apiVersion: '2010-03-31'}).publish(parameters).promise();
+    console.log('sent message with id: ' + publishedtext.MessageId);
+    // created promise and SNS service object
+  } catch (e) {
+    console.error('[ ' + Date() + ' ] ', e);
+  };
+}
+// sent a message.
 
 
 

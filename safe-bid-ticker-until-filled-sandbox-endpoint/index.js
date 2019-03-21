d947efb2a73c0395c3c5a85048b985ed3d652fc6
munair <munair@gmail.com>
2019-03-21 13:22:26 +0900

    finally... 'done'

--- safe-bid-ticker-until-filled-sandbox-endpoint/index.js
+++ safe-bid-ticker-until-filled-sandbox-endpoint/index.js
@@ -345,14 +345,14 @@ async function sendmessage(message, phonenumber) {
                             + ' [order submission: ' + orderquantity + ' ' + basecurrency + ' @ ' + orderprice + ' ' + basecurrency + '/' + quotecurrency + ']');
       } // updated console.
 
-      if ( orderstatus === 'filled' || orderstatus === 'rejected' ) { // discontinue subscription if order filled or rejected...
+      if ( orderstatus === 'done' || orderstatus === 'rejected' ) { // discontinue subscription if order filled or rejected...
         if ( orderstatus === 'rejected' ) { console.log('order for ' + bidquantity + ' ' + basecurrency + ' @ ' + bidprice + ' ' + basecurrency + '/' + quotecurrency + ' rejected.'); }
         if ( orderstatus === 'filled' ) { console.log('order filled.'); }
         let subscriptionrequest = channelsubscription('unsubscribe', productid, channel, signature, key, passphrase);
         try { ws.send(JSON.stringify(subscriptionrequest)); } catch (e) { console.error(e); }
       } // discontinued subscription.
 
-      if ( orderstatus === 'filled' ) { // make ask.
+      if ( orderstatus === 'done' ) { // make ask.
         // always add the quote increment to ensure that the ask is never rejected for being the same as the bid.
         let askprice = Math.round( Number(quoteincrement) + orderprice * ( 1 + percentreturn ) / quoteincrement ) * quoteincrement;
         let askquantity = orderquantity;

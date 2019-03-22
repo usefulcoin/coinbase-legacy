347e310d19ee6b7d32ceb915c75dc50be46f3553
munair <munair@gmail.com>
2019-03-23 08:02:10 +0900

    try again. and again.

--- safe-bid-ticker-until-filled-sandbox-endpoint/index.js
+++ safe-bid-ticker-until-filled-sandbox-endpoint/index.js
@@ -27,7 +27,7 @@ const fetch = require('node-fetch');
 
 
 // define consts...
-const channels = '["level2","match"]';
+const channels = ['level2','match'];
 const riskratio = 0.00001;
 const percentreturn = 0.01;
 const productid = 'BTC-USD';

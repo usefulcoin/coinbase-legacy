b02402c0f7a195a24e6d89f718dabb91b8ddb246
munair <munair@gmail.com>
2019-03-23 00:45:16 +0900

    rollback

--- safe-bid-ticker-until-filled-sandbox-endpoint/index.js
+++ safe-bid-ticker-until-filled-sandbox-endpoint/index.js
@@ -369,19 +369,30 @@ async function sendmessage(message, phonenumber) {
         if ( newbidquantity > basemaximum ) { newbidquantity = basemaximum } /* make sure that the new bid quantity is within Coinbase bounds... */
         newbidprice = Number(newbidprice).toFixed(Math.abs(Math.log10(quoteincrement))); /* make absolutely sure that it is rounded and of a fixed number of decimal places. */
         newbidquantity = Number(newbidquantity).toFixed(Math.abs(Math.log10(baseminimum))); /* make absolutely sure that it is rounded and of a fixed number of decimal places. */
-        let biddelta = bidprice - newbidprice;
+        let delta = bidprice - newbidprice;
         if ( sidechange === 'sell' ) { // inspect updated sell offer.
-          if ( Math.abs(biddelta) === 0 ) { // check if the best ask price matches submitted price (which subtracted the quote increment from the best sell price).
+          if ( Math.abs(delta) === 0 ) { // check if the best ask price matches submitted price (which subtracted the quote increment from the best sell price).
             let orderinformation; try { orderinformation = await restapirequest('GET','/orders/' + bidid); } catch (e) { console.error(e); }
             if ( Object.keys(orderinformation) === 'message' ) { messagehandlerexit('l2update',sidechange.padStart(5) + ' ' + formattedsize + ' @ ' + formattedprice,orderinformation.message); }
             if ( Object.keys(orderinformation).length === 0 ) { messagehandlerexit('l2update',sidechange.padStart(5) + ' ' + formattedsize + ' @ ' + formattedprice,'bad request'); }
             else { // handle non-null response from rest api server returned.
               bidstatus = orderinformation.status; 
               bidfilled = orderinformation.filled_size; 
-              messagehandlerinfo('l2update',sidechange.padStart(5) + ' ' + formattedsize + ' @ ' + formattedprice,'existing bid status: (~' + bidfilled + ') ' + bidstatus);
+              newbidquantity = quoteriskablebalance/newbidprice - bidfilled; /* defined safe (riskable) bid quantity */
+              if ( newbidquantity < baseminimum ) { newbidquantity = baseminimum } /* make sure that the new bid quantity is within Coinbase bounds... */
+              if ( newbidquantity > basemaximum ) { newbidquantity = basemaximum } /* make sure that the new bid quantity is within Coinbase bounds... */
+              newbidquantity = Number(newbidquantity).toFixed(Math.abs(Math.log10(baseminimum))); /* make absolutely sure that it is rounded and of a fixed number of decimal places. */
+              messagehandlerinfo('l2update',sidechange.padStart(5) + ' ' + formattedsize + ' @ ' + formattedprice,'existing bid status: (~' + newbidquantity + ') ' + bidstatus);
+              // if ( bidstatus = 'open' ) { try { await restapirequest('DELETE','/orders/' + orderid); } catch (e) { console.error(e); } }
+              // if ( bidstatus = 'open' ) { let orderinformation; try { orderinformation = await postorder(bidprice,bidquantity,'buy',true,productid); } catch (e) { console.error(e); } }
+              // bidid = orderinformation.id;
+              // bidstatus = orderinformation.status; 
+              // bidfilled = orderinformation.filled_size; 
             } // handled non-null response from rest api server returned.
           } // checked if the best ask price matches submitted price.
+          else { messagehandlerinfo('l2update',sidechange.padStart(5) + ' ' + formattedsize + ' @ ' + formattedprice,newbidquantity + ' @ ' + newbidprice); }
         } // inspected updated sell offer.
+        else { messagehandlerinfo('l2update',sidechange.padStart(5) + ' ' + formattedsize + ' @ ' + formattedprice,newbidquantity + ' @ ' + newbidprice); } /* log bid offer information to console */
       } // updated bid.
     } // handled each level2 update.
   }); // end handling websocket messages.

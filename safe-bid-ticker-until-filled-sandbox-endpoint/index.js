3f62b350546c551114425e63d2d69651b11286d6
munair <munair@gmail.com>
2019-03-22 11:08:52 +0900

    jeezup... massive logic errors and if forgot the LENGTH. argh.

--- safe-bid-ticker-until-filled-sandbox-endpoint/index.js
+++ safe-bid-ticker-until-filled-sandbox-endpoint/index.js
@@ -302,17 +302,17 @@ async function sendmessage(message, phonenumber) {
         if ( bidquantity > basemaximum ) { bidquantity = basemaximum } /* make sure bid quantity is within Coinbase bounds... */
         bidquantity = Number(bidquantity).toFixed(Math.abs(Math.log10(baseminimum))); /* make absolutely sure that it is rounded and of a fixed number of decimal places. */
         try { bidinformation = await postorder(bidprice,bidquantity,'buy',true,productid); } catch (e) { console.error(e); }
+        if ( Object.keys(bidinformation) === 'message' ) {  // discontinue subscription if error message received from rest api server.
+          messagehandlerexit('snapshot',snapshotsize + ' @ ' + snapshotprice,orderinformation.message);
+        } // discontinued subscription.
         if ( Object.keys(bidinformation).length === 0 ) { // discontinue subscription if bad bid.
           messagehandlerexit('snapshot',snapshotsize + ' @ ' + snapshotprice,'bad bid: ' + bidquantity + ' ' + basecurrency + ' @ ' + bidprice + ' ' + basecurrency + '/' + quotecurrency);
-        } // discontinued subscription.
-        else if ( Object.keys(bidinformation) === 'message' ) {  // discontinue subscription if error message received from rest api server.
-          messagehandlerexit('snapshot',snapshotsize + ' @ ' + snapshotprice,orderinformation.message);
         } // discontinued subscription.
         else {
           if ( bidinformation.status === 'rejected' ) { // discontinue subscription if bid rejected.
             messagehandlerexit('snapshot',snapshotsize + ' @ ' + snapshotprice,'rejected bid: ' + bidquantity + ' ' + basecurrency + ' @ ' + bidprice + ' ' + basecurrency + '/' + quotecurrency);
           } // discontinued subscription if bid rejected. 
-          if ( bidinformation.id === 36 ) { // valid order submitted. update state variables.
+          if ( bidinformation.id.length === 36 ) { // valid order submitted. update state variables.
             bidid = bidinformation.id;
             bidfilled = bidinformation.filled_size;
             bidstatus = bidinformation.status;

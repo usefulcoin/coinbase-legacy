db46ea35180bbf3b1c0715c12ce1b39ddc5115bf
munair <munair@gmail.com>
2019-03-22 10:06:47 +0900

    rollback

--- safe-bid-ticker-until-filled-sandbox-endpoint/index.js
+++ safe-bid-ticker-until-filled-sandbox-endpoint/index.js
@@ -354,9 +354,9 @@ async function sendmessage(message, phonenumber) {
           newbidprice = Number(newbidprice).toFixed(Math.abs(Math.log10(quoteincrement))); /* make absolutely sure that it is rounded and of a fixed number of decimal places. */
           newbidquantity = Number(newbidquantity).toFixed(Math.abs(Math.log10(baseminimum))); /* make absolutely sure that it is rounded and of a fixed number of decimal places. */
           console.log( bidprice + ' !== ' + newbidprice ); if ( bidprice !== newbidprice ) { // check for a change in the best ask price.
-            try { bidinformation = await restapirequest('GET','/bids/' + bidid); } catch (e) { console.error(e); }
-            orderstatus = bidinformation.status; /* update orderstatus information for submitted bid */
-            console.log('orderstatus: ' + orderstatus);
+            let orderinformation; try { orderinformation = await restapirequest('GET','/orders/' + bidid); } catch (e) { console.error(e); }
+            orderstatus = orderinformation.status; /* update orderstatus information for submitted bid */
+            console.log(orderstatus);
           } // checked for a change in the best ask price.
         } // inspected updated sell offer.
         else { // no change in best buy offer. so just report it to the console.

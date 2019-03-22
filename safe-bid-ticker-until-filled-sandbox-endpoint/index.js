72a6769a58eb7aafcbaaf733a76a715e5449295c
munair <munair@gmail.com>
2019-03-23 00:11:16 +0900

    back at square one.

--- safe-bid-ticker-until-filled-sandbox-endpoint/index.js
+++ safe-bid-ticker-until-filled-sandbox-endpoint/index.js
@@ -371,8 +371,6 @@ async function sendmessage(message, phonenumber) {
         newbidquantity = Number(newbidquantity).toFixed(Math.abs(Math.log10(baseminimum))); /* make absolutely sure that it is rounded and of a fixed number of decimal places. */
         if ( sidechange === 'sell' ) { // inspect updated sell offer.
           let biddelta = bidprice - newbidprice;
-          let pricedelta = bidprice - formattedprice;
-          console.log(biddelta);
           if ( Math.abs(biddelta) === 0 ) { // check if the best ask price matches submitted price (which subtracted the quote increment).
             let orderinformation; try { orderinformation = await restapirequest('GET','/orders/' + bidid); } catch (e) { console.error(e); }
             if ( Object.keys(orderinformation) === 'message' ) { messagehandlerexit('l2update',sidechange.padStart(5) + ' ' + formattedsize + ' @ ' + formattedprice,orderinformation.message); }
@@ -384,7 +382,7 @@ async function sendmessage(message, phonenumber) {
               newbidquantity = Number(newbidquantity).toFixed(Math.abs(Math.log10(baseminimum))); /* make absolutely sure that it is of a fixed number of decimal places. */
               messagehandlerinfo('l2update',sidechange.padStart(5) + ' ' + formattedsize + ' @ ' + formattedprice,'existing bid status: (~' + newbidquantity + ') ' + bidstatus);
           } // checked if the best ask price matches submitted price.
-          if ( Math.abs(biddelta) > Number(quoteincrement) ) { // check for a change in the best ask price exceeding the quote increment.
+          if ( Math.abs(biddelta) > 0 ) { // check for a change in the best ask price exceeding the quote increment.
               // if ( bidstatus = 'open' ) { try { await restapirequest('DELETE','/orders/' + orderid); } catch (e) { console.error(e); } }
               // if ( bidstatus = 'open' ) { let orderinformation; try { orderinformation = await postorder(newbidprice,newbidquantity,'buy',true,productid); } catch (e) { console.error(e); } }
               // bidid = orderinformation.id;

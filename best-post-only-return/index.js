b2a4319f8d6a2e32495b54f07a9ff833a1bdc3e1
munair <munair@gmail.com>
2019-03-24 23:32:47 +0900

    standardize and take a pass at improving legibility.

--- best-post-only-return/index.js
+++ best-post-only-return/index.js
@@ -16,19 +16,19 @@
 
 
 
-// load modules...
+// load modules.
 const aws = require('aws-sdk');
 const websocket = require('ws');
 const crypto = require('crypto');
 const fetch = require('node-fetch');
-// loading complete.
+// loaded modules.
 
 
 
 
-// define consts...
+// define consts.
 const channels = ['heartbeat','full','level2'];
-const riskratio = 0.00001;
+const riskratio = 0.01;
 const percentreturn = 0.01;
 const productid = 'BTC-USD';
 const recipient = '+12062270634';
@@ -38,16 +38,16 @@ const ws = new websocket('wss://ws-feed-public.sandbox.prime.coinbase.com');
 
 
 
-// import sensitive data...
+// import sensitive data.
 const key = process.env.apikey;
 const secret = process.env.apisecret;
 const passphrase = process.env.apipassphrase;
-// importing of sensitive authentication data complete.
+// imported sensitive authentication data.
 
 
 
 
-// declare quasi-persistent websocket variables...
+// declare quasi-persistent websocket variables.
 let askid;
 let bidid;
 let askprice;
@@ -57,41 +57,41 @@ let bidquantity;
 let asksuccess;
 let bidsuccess;
 let subscribed;
-let orderconfiguration = new Object();
+let orderscope = new Object();
 // declared quasi-persistent websocket variables.
 
-// filter an array of objects...
-function filter(array, filters) {
+
+
+
+function filter (array, filters) { // filter an array of objects.
   let itemstoinclude = Object.keys(filters);
   return array.filter((item) => itemstoinclude.every((key) => (filters[key].indexOf(item[key]) !== -1)));
-}
-// filtered array.
+} // filtered array.
 
 
 
 
-// make rest api request...
-async function restapirequest(method,requestpath,body){
+async function restapirequest ( method, requestpath, body ) { // make rest api request.
  
-  // create the prehash string by concatenating required parts of request...
+  // create the prehash string by concatenating required parts of request.
   let timestamp = Date.now() / 1000;
   let prehash = timestamp + method + requestpath;
   if ( body !== undefined ) { prehash = prehash + body; }
   // created the prehash.
  
-  // base64 decode the secret...
+  // base64 decode the secret.
   let base64decodedsecret = Buffer(secret, 'base64');
   // secret decoded.
  
-  // create sha256 hmac with the secret...
+  // create sha256 hmac with the secret.
   let hmac = crypto.createHmac('sha256',base64decodedsecret);
   // created sha256 hmac.
  
-  // sign the require message with the hmac and base64 encode the result..
+  // sign the require message with the hmac and base64 encode the result.
   let signedmessage = hmac.update(prehash).digest('base64');
   // signed message.
  
-  // define coinbase required headers...
+  // define coinbase required headers.
   let headers = {
     'ACCEPT': 'application/json',
     'CONTENT-TYPE': 'application/json',
@@ -103,28 +103,27 @@ async function restapirequest(method,requestpath,body){
   // defined coinbase required headers. yes... content-type is required.
   // see https://docs.prime.coinbase.com/#requests for more information.
 
-  // define request options for http request...
+  // define request options for http request.
   let requestoptions = { 'method': method, headers };
   if ( body !== undefined ) { requestoptions['body'] = body; }
   // defined request options for http request.
 
-  // define url and send request...
+  // define url and send request.
   let url = 'https://api-public.sandbox.prime.coinbase.com' + requestpath;
   let response = await fetch(url,requestoptions);
   let json = await response.json();
   // defined url and sent request.
 
   return json;
-}
-// made rest api request.
+
+} // made rest api request.
 
 
 
 
-// post a order...
-async function postorder(price,size,side,postonly,productid,stop,stopprice){
+async function postorder ( price, size, side, postonly, productid, stop, stopprice ) { // post a order.
 
-  // define parameters...
+  // define parameters.
   let method = 'POST';
   let requestpath = '/orders';
   let body = JSON.stringify({
@@ -138,45 +137,44 @@ async function postorder(price,size,side,postonly,productid,stop,stopprice){
   });
   // defined parameters.
 
-  let order = await restapirequest(method,requestpath,body);
+  let order = await restapirequest ( method, requestpath, body );
 
   return order;
-}
-// posted a order.
+
+} // posted a order.
 
 
 
 
-// sign request...
-function signrequest(method,requestpath,body){
+function signrequest ( method, requestpath, body ) { // sign request.
 
-  // create the prehash string by concatenating required parts of request...
+  // create the prehash string by concatenating required parts of request.
   let timestamp = Date.now() / 1000;
   let prehash = timestamp + method + requestpath;
   if ( body !== undefined ) { prehash = prehash + body; }
   // created the prehash.
 
-  // base64 decode the secret...
-  let base64decodedsecret = Buffer(secret, 'base64');
+  // base64 decode the secret.
+  let base64decodedsecret = Buffer( secret, 'base64' );
   // secret decoded.
 
-  // create sha256 hmac with the secret...
+  // create sha256 hmac with the secret.
   let hmac = crypto.createHmac('sha256',base64decodedsecret);
   // created sha256 hmac.
 
-  // sign the require message with the hmac and base64 encode the result..
+  // sign the require message with the hmac and base64 encode the result.
   let signedmessage = hmac.update(prehash).digest('base64');
   // signed message.
 
   return { 'signedmessage': signedmessage, 'timestamp': timestamp };
-}
-// signed request.
+
+} // signed request.
 
 
 
 
-// create or discontinue subscription request...
-function channelsubscription(type, productid, channels, signature, key, passphrase) {
+function channelsubscription ( type, productid, channels, signature, key, passphrase ) { // create or discontinue subscription request...
+
   let subscriptionrequest = {
       'type': type,
       'product_ids': [productid],
@@ -186,41 +184,40 @@ function channelsubscription(type, productid, channels, signature, key, passphra
       'passphrase': passphrase,
       'timestamp': signature.timestamp
   }
+
   return subscriptionrequest;
-}
-// created or discontinued subscription request.
+
+} // created or discontinued subscription request.
 
 
 
 
-// configure aws region (use ap-northeast-1 or us-west-2)...
-aws.config.update({region: 'ap-northeast-1'});
+// configure aws region (use ap-northeast-1 or us-west-2).
+aws.config.update( { region: 'us-west-2' } );
 // configured aws.
 
 
 
 
-// send a message...
-async function sendmessage(message, phonenumber) {
+async function sendmessage ( message, phonenumber ) { // send a message.
+
   // create publish parameters...
   let parameters = { Message: message, PhoneNumber: phonenumber };
   // create publish parameters.
 
-  try{
-    // create promise and SNS service object...
-    let publishedtext = await new aws.SNS({apiVersion: '2010-03-31'}).publish(parameters).promise();
-    console.log('sent message with id: ' + publishedtext.MessageId);
-    // created promise and SNS service object
-  } catch (e) {
-    console.error('[ ' + Date() + ' ] ', e);
-  };
-}
-// sent a message.
+  // create promise and SNS service object.
+  let publishedtext; try{ publishedtext = await new aws.SNS( {apiVersion: '2010-03-31'} ).publish( parameters ).promise(); } catch (e) { console.error(e); }
+  // created promise and SNS service object
+
+  console.log('sent message with id: ' + publishedtext.MessageId);
+
+} // sent a message.
 
 
 
 
-async function configureorder(productid) {
+async function scopeorder ( productid ) { // scope order.
+
   // declare variables.
   let baseminimum;
   let basemaximum;
@@ -230,8 +227,11 @@ async function configureorder(productid) {
   let quoteriskablebalance;
   // declared variables.
 
-  // retrieve product information...
+  // retrieve product information.
   let productinformation; try { productinformation = await restapirequest('GET','/products/' + productid); } catch (e) { console.error(e); }
+  // retrieved product information.
+
+  // verify response.
   if ( Object.keys(productinformation).length === 0 ) { errormessage = 'unable to retrieve ' + productid + ' product information'; }
   else if ( Object.keys(productinformation).length === 1 ) { errormessage = 'the Coinbase response is "' + productinformation.message + '"'; }
   else {
@@ -240,20 +240,21 @@ async function configureorder(productid) {
     basecurrency = productinformation.base_currency;
     quotecurrency = productinformation.quote_currency;
     quoteincrement = productinformation.quote_increment;
-  }
-  // retrieved product information.
+  } // verified response.
 
-  // retrieve available balance information...
+  // retrieve available balance information.
   let quotecurrencyfilter = { currency: [quotecurrency] };
   let accountinformation; try { accountinformation = await restapirequest('GET','/accounts'); } catch (e) { console.error(e); }
+  // retrieved account balance information.
+
+  // verify response.
   if ( Object.keys(accountinformation).length === 0 ) { errormessage = 'unable to retrieve account information'; }
   else if ( Object.keys(accountinformation).length === 1 ) { errormessage = 'the Coinbase response is "' + accountinformation.message + '"'; }
   else {
     let quoteaccountinformation = filter(accountinformation, quotecurrencyfilter);
     let quoteavailablebalance = quoteaccountinformation[0].available;
     quoteriskablebalance = quoteavailablebalance*riskratio;
-  }
-  // retrieved account balance information.
+  } // verified response.
 
   let configurationinformation = { // make configuration information object.
     'baseminimum': baseminimum,
@@ -265,12 +266,14 @@ async function configureorder(productid) {
   } // made configuration information object.
 
   return configurationinformation;
-}
+
+} // scoped order.
 
 
 
 
-async function makeask(bidprice,bidquantity,configurationinformation) {
+async function makeask ( bidprice, bidquantity, configurationinformation ) {
+
   // declare variables.
   let baseminimum = configurationinformation.baseminimum;
   let basemaximum = configurationinformation.basemaximum;
@@ -302,21 +305,23 @@ async function makeask(bidprice,bidquantity,configurationinformation) {
   else { errormessage = 'unexpected error encountered submitting ask: ' + askquantity + ' ' + basecurrency + ' @ ' + askprice + ' ' + basecurrency + '/' + quotecurrency; } 
   // analyze response.
 
-  let asksubmission = {
+  let asksubmission = { make output object.
     'askid': askinformation.id,
     'askprice': askinformation.price,
     'askquantity': askinformation.quantity,
     'successmessage': successmessage,
     'errormessage': errormessage
-  }
+  } made output object.
 
   return asksubmission;
+
 }
 
 
 
 
-async function makebid(askprice,askquantity,configurationinformation) {
+async function makebid ( askprice, askquantity, configurationinformation ) {
+
   // declare variables.
   let snapshotprice = askprice; /* capture best ask price from the orderbook. */
   let snapshotsize = askquantity; /* capture best ask quantity from the orderbook. */
@@ -351,15 +356,16 @@ async function makebid(askprice,askquantity,configurationinformation) {
   else { errormessage = 'unexpected error encountered submitting bid: ' + bidquantity + ' ' + basecurrency + ' @ ' + bidprice + ' ' + basecurrency + '/' + quotecurrency; } 
   // analyze response.
 
-  let bidsubmission = {
+  let bidsubmission = { make output object.
     'bidid': bidinformation.id,
     'bidprice': bidinformation.price,
     'bidquantity': bidinformation.quantity,
     'successmessage': successmessage,
     'errormessage': errormessage
-  }
+  } made output object.
 
   return bidsubmission;
+
 }
 
 
@@ -367,21 +373,33 @@ async function makebid(askprice,askquantity,configurationinformation) {
 
 (async function main() {
 
-  // create signature required to subscribe to a channel...
+
+
+
+  // create signature required to subscribe to a channel.
   let signature = signrequest('GET','/users/self/verify');
   // created signature required to subscribe to a channel.
+
+
+
 
   // update console on close connection...
   ws.on('close', function close() { console.log('disconnected'); });
   // updated console on close connection.
 
-  // on open connection and send subscribe request...
+
+
+
+  // on open connection and send subscribe request.
   ws.on('open', function open() {
     console.log('connected');
     let subscriptionrequest = channelsubscription('subscribe', productid, channels, signature, key, passphrase);
     try { ws.send(JSON.stringify(subscriptionrequest)); } catch (e) { console.error(e); }
   });
   // opened connection and sent subscribe request.
+
+
+
 
   ws.on('message', async function incoming(data) { // start handling websocket messages.
     let jsondata = JSON.parse(data);
@@ -416,10 +434,10 @@ async function makebid(askprice,askquantity,configurationinformation) {
         let snapshotsize = jsondata.asks[0][1]; /* capture best ask quantity from the orderbook. */
 
         // retrieve REST API parameters.
-        orderconfiguration = await configureorder(productid);
+        orderscope = await scopeorder(productid);
         // retrieved REST API parameters.
 
-        let bid = await makebid(snapshotprice, snapshotsize, orderconfiguration);
+        let bid = await makebid(snapshotprice, snapshotsize, orderscope);
         bidid = bid.bidid;
         bidsuccess = bid.successmessage;
         biderror = bid.errormessage;
@@ -429,7 +447,6 @@ async function makebid(askprice,askquantity,configurationinformation) {
         if ( bidsuccess ) { messagehandlerinfo('snapshot',snapshotsize + ' @ ' + snapshotprice,bidsuccess); }
       } // made bid.
     } // handled level2 snapshot message.
-
 
     if ( jsondata.type === 'done' ) { // handle done message from the full channel.
       let id = jsondata.order_id;
@@ -441,7 +458,7 @@ async function makebid(askprice,askquantity,configurationinformation) {
       if ( id === bidid ) { 
         messagehandlerinfo('done','order (id: ' + id + ') ' + reason,remaining + ' remaining to ' + side + ' at ' + price + ' [' + pair + ']'); 
         if ( reason === 'filled' ) {
-          let ask = await makeask(bidprice, bidquantity, orderconfiguration); /* this function takes the bid price and bid quantity as inputs */
+          let ask = await makeask(bidprice, bidquantity, orderscope); /* this function takes the bid price and bid quantity as inputs */
           askid = ask.askid;
           askprice = ask.askprice;
           askquantity = ask.askquantity;
@@ -458,4 +475,8 @@ async function makebid(askprice,askquantity,configurationinformation) {
       }
     } // handled done message from the full channel.
   }); // end handling websocket messages.
+
+
+
+
 }());

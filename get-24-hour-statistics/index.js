54ad76e3018b99d99d981bd97b104e04ebefff6b
munair <munair@gmail.com>
2019-03-26 01:35:56 +0900

    was using the wrong data.

--- get-24-hour-statistics/index.js
+++ get-24-hour-statistics/index.js
@@ -174,7 +174,7 @@ let orderscope = await scopeorder(productid);
 // report.
 let price = Number ( statistics.last );
 let volume = Number ( statistics.volume );
-let averagevolume = Number ( statistics.volume ) / 30;
+let averagevolume = Number ( statistics.volume_30day ) / 30;
 let range = Number ( statistics.high ) - Number ( statistics.low );
 let offhigh = Number ( statistics.high ) - Number ( statistics.last );
 let offlow = Number ( statistics.last ) - Number ( statistics.low );

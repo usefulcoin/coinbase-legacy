4fed53bc836208a47826f4e1147716cad85067cf
munair <munair@gmail.com>
2019-03-26 00:06:27 +0900

    rollback

--- post-bid-and-return-yielding-ask/index.js
+++ post-bid-and-return-yielding-ask/index.js
@@ -433,7 +433,6 @@ async function makebid ( askprice, askquantity, configurationinformation ) {
 
         // retrieve REST API parameters.
         orderscope = await scopeorder(productid);
-        console.log(orderscope);
         // retrieved REST API parameters.
 
         // make bid.

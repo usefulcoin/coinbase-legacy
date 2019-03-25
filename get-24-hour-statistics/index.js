1b7fdbbfbb461099b5ce9cca234d75a23ad51fcb
munair <munair@gmail.com>
2019-03-26 06:31:32 +0900

    debugging

--- get-24-hour-statistics/index.js
+++ get-24-hour-statistics/index.js
@@ -173,8 +173,8 @@ else {
 
   // report.
   let price = Number ( statistics.last );
-  let volume = Number ( statistics.volume ) * Number ( price );
-  let averagevolume = Number ( statistics.volume_30day ) * Number ( price ) / 30;
+  let volume = Number ( statistics.volume ) * Number ( statistics.last );
+  let averagevolume = Number ( statistics.volume_30day ) * Number ( statistics.last ) / 30;
   let range = Number ( statistics.high ) - Number ( statistics.low );
   let offhigh = Number ( statistics.high ) - Number ( statistics.last );
   let offlow = Number ( statistics.last ) - Number ( statistics.low );
